a345e91c59bff2f181bd39601e73229f6a375eaf
helo1313 <helo1313@wp.pl>
2021-11-06 23:37:20 +0100

    Project Setup

---
+++ Source/Minecraft/Block.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Block.h"
+
+// Sets default values
+ABlock::ABlock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SM_Block = CreateDefaultSubobject<UStaticMeshComponent>("Block Mesh");
+	SetRootComponent(SM_Block);
+
+}
+
+// Called when the game starts or when spawned
+void ABlock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/Minecraft/Block.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Block.generated.h"
+
+UCLASS()
+class MINECRAFT_API ABlock : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABlock();
+
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMeshComponent* SM_Block;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+};
